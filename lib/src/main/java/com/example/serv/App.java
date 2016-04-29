54268c4edac74dc9940ec27af5e1a6c32336000a
Aaron Taylor <Aaron Taylor>
2016-04-28 21:16:57 -0700

    fuk this

--- lib/src/main/java/com/example/serv/App.java
+++ lib/src/main/java/com/example/serv/App.java
@@ -10,7 +10,7 @@ public class App {
 
         System.out.println("Sending POST to GCM");
 
-        String apiKey = "AIzaSyC_oi5v-QwyHirz_xbDzwcdcAROWrZ-xwc";
+        String apiKey = "AIzaSyDLCuqcc_RXK80Z8ttt-eA86gvpH-Po1SQ";
         Content content = createContent();
 
         Post2Gcm.post(apiKey, content);
@@ -19,7 +19,7 @@ public class App {
     public static Content createContent() {
         Content c = new Content();
 
-        c.addRegId("APA91bGB-ZbeyCtLP_QRNQE5zu4Y-1LgRlTFiIMh2vDrhtgVwL5vTo5DYiE_YFo4ftOjxk4Nihykt_LSTPL9T_7FX64I7EQoe-l_eQ_4-T36BwAdOgAioktNUE1fwFoaPe6fdzQ1Aayx");
+        c.addRegId("APA91bE1B9b-HGdXbCGkihEEite2nt_Jl5YsPwxkupKX4IAtda2yjpPL5emwJ4DaAIicSIo7SJutnrW9EexKqQddYCCDdPCi2bb0O6pDCvitpkkD5dmXloLeNYoogzKAJqfFMgyicx2_");
 
         c.createData("Working!!", "Test message");
 
